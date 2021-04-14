a97d693c1b4f7df4e3a96233e3905936ecb6bd2d
Karanbisht511 <karanbisht511@gmai.com>
2021-04-14 20:22:29 +0530

    chatbot deploy

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "public/index");
+});
+
+app.listen(port, function () {
+    console.log("server is running on port:" + port);
+});
