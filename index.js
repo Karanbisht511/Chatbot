7f46c2a6f0479f3f5ba7246619e3a4dec4c40fc9
Karanbisht511 <karanbisht511@gmai.com>
2021-04-14 20:53:59 +0530

    update

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const express = require('express');
 const app = express();
 const port = 3000 || process.env.PORT;
 
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    res.sendFile("index");
+    res.sendFile(__dirname + "/publc/index");
 });
 
 app.listen(port, function () {
